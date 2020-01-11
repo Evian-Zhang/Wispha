d03d26e89c20bd1e76f47114d787972cf016c3a5
Evian-Zhang <evianzhang1999@163.com>
2020-01-12 00:50:55 +0800

    complete a rough serialization with an ugly way

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -2,33 +2,52 @@ use std::collections::HashMap;
 use std::path::PathBuf;
 use std::sync::{Arc, Mutex, Weak};
 use std::hash::{Hash, Hasher};
+use std::fmt::Display;
 
 use chrono::{DateTime, Utc};
-use serde::ser::{Serializer, SerializeStruct};
+use serde::ser::{Serializer, SerializeStruct, SerializeMap};
 use serde::{Serialize, Deserialize};
+use serde::export::Formatter;
+use serde::export::fmt::Error;
 
 type NodePathComponents = Vec<String>;
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct NodePath<'a> {
     pub components: NodePathComponents,
     pub tree: &'a Tree<'a>,
 }
 
-impl<'a> Hash for NodePath<'a> {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        self.components.hash(state);
+impl<'a> NodePath<'a> {
+    pub fn to_string(&self) -> String {
+        // TODO: add a singleton String "/" instead of creating a String every time
+        "/".to_owned() + &self.components.join("/")
     }
 }
 
+impl Display for NodePath<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
+        write!(f, "{}", self.to_string())
+    }
+}
+
+//impl Serialize for NodePath<'_> {
+//    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+//        where
+//            S: Serializer {
+//        serializer.serialize_str(&self.to_string())
+//    }
+//}
+
 /// The properties that are related to the node itself, but not the truly valuable information.
-#[derive(Clone, Serialize)]
+#[derive(Clone, Serialize, Debug)]
 pub struct NodeProperties<'a> {
     pub name: String,
     #[serde(skip)]
     pub record_file: NodePath<'a>,
 }
 
+#[derive(Debug)]
 pub enum PropertyType {
     String,
     Date,
@@ -37,7 +56,7 @@ pub enum PropertyType {
 }
 
 /// The property with type. Support `String`, `Date` (stores in UTC), `Int` (with the same range as `isize` of Rust), `Double` (with the same range and precision as `f64` of Rust)
-#[derive(Serialize)]
+#[derive(Serialize, Debug)]
 pub enum TypedProperty {
     String(String),
 
@@ -74,25 +93,40 @@ mod date_format {
 }
 
 /// Direct node structure, i.e. the node that truly has valuable values
-#[derive(Serialize)]
+#[derive(Debug)]
 pub struct DirectNode<'a> {
     /// If a Wispha node doesn't have parent (for example, `root` in a Wispha tree), this field is `None`
-    pub parent: Option<PathBuf>,
+    pub parent: Option<NodePath<'a>>,
 
     /// If a Wispha node doesn't have any child, this field is an vector with length 0
-    #[serde(skip_serializing_if = "std::vec::Vec::is_empty", serialize_with = "")]
-    pub children: Vec<PathBuf>,
+    pub children: Vec<NodePath<'a>>,
 
     /// The properties that are related to the node itself, but not the truly valuable information.
-    #[serde(flatten)]
     pub node_properties: NodeProperties<'a>,
 
     /// Customized properties in a direct node, supporting `String`, `Date`, `Int` and `Double`.
-    #[serde(flatten)]
     pub properties: HashMap<String, TypedProperty>,
 }
 
-#[derive(Serialize)]
+impl<'a> Serialize for DirectNode<'a> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer {
+        // TODO: find a more graceful way
+        let mut map = serializer.serialize_map(None)?;
+        map.serialize_entry("name", &self.node_properties.name)?;
+        for (property, value) in self.properties.iter() {
+            map.serialize_entry(property, value)?;
+        }
+        if !self.children.is_empty() {
+            let children: Vec<Arc<Mutex<Node>>> = self.children.iter().map(|node_path| node_path.tree.get_node(node_path).unwrap()).collect();
+            map.serialize_entry("children", &children)?;
+        }
+        map.end()
+    }
+}
+
+#[derive(Serialize, Debug)]
 pub struct LinkNode<'a> {
     /// The properties that are related to the node itself, but not the truly valuable information.
     #[serde(flatten)]
@@ -100,7 +134,7 @@ pub struct LinkNode<'a> {
 }
 
 /// Wispha node structure
-#[derive(Serialize)]
+#[derive(Serialize, Debug)]
 pub enum Node<'a> {
     Direct(DirectNode<'a>),
     Link(LinkNode<'a>),
@@ -116,44 +150,29 @@ impl<'a> Node<'a> {
     }
 }
 
-//impl Serialize for Node {
-//    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-//        where
-//            S: Serializer {
-//        use Node::*;
-//        let mut state = serializer.serialize_struct("Node", 0)?;
-//        match &self {
-//            Direct(direct_node) => {
-//                state.serialize_field("name", &direct_node.node_properties.name);
-//
-//            },
-//            Link(link_node) => {
-//                state.serialize_field("name", &link_node.node_properties.name);
-//
-//            }
-//        }
-//        state.end()
-//    }
-//}
-
 /// Wispha tree structure
+#[derive(Debug)]
 pub struct Tree<'a> {
     pub nodes: HashMap<NodePathComponents, Arc<Mutex<Node<'a>>>>,
     pub root: Weak<Mutex<Node<'a>>>,
     pub custom_properties: HashMap<String, PropertyType>,
 }
 
-impl Serialize for Tree {
+impl Serialize for Tree<'_> {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
         where
             S: Serializer {
         if !self.custom_properties.is_empty() {
-            let mut state = serializer.serialize_struct("Tree", 0)?;
-            let root = self.root.upgrade().unwrap();
-            state.serialize_field("name", &root.lock().unwrap().node_properties().name)?;
-            state.end()
+            self.root.upgrade().unwrap().lock().unwrap().serialize(serializer)
         } else {
             serializer.serialize_none()
         }
+    }
+}
+
+impl<'a> Tree<'a> {
+    pub fn get_node(&self, components: &NodePath) -> Option<Arc<Mutex<Node<'a>>>> {
+        self.nodes.get(&components.components)
+            .map(|node_ref| Arc::clone(node_ref))
     }
 }
